73876dbe7b5e193f35f09159fa67c1b038a6358a
Ivan Zaytsev <ivzaycev0717@yandex.ru>
2024-05-02 16:00:04 +0300

    login and registration

--- mct_app/auth/forms.py
+++ mct_app/auth/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 import phonenumbers
-from wtforms import StringField, PasswordField, EmailField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, EmailField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
 
-class LoginForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
-    email = EmailField('Адрес электронной почты', validators=[Email()])
-    phone = StringField('Номер телефона', validators=[DataRequired()])
-    submit = SubmitField('Войти')
+    email = EmailField('Адрес электронной почты', validators=[DataRequired(), Email()])
+    phone = StringField('Номер телефона', validators=[])
+    submit = SubmitField('Зарегистрироваться')
 
     def validate_phone(self, phone):
         try:
@@ -17,3 +17,8 @@ class LoginForm(FlaskForm):
                 raise ValueError()
         except (phonenumbers.phonenumberutil.NumberParseException, ValueError):
             raise ValidationError('Неправильно введен номер телефона')
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
--- mct_app/auth/views.py
+++ mct_app/auth/views.py
@@ -1,14 +1,30 @@
-from flask import Blueprint, render_template, session, redirect, url_for
+from flask import Blueprint, render_template, session, redirect, url_for, flash
 from mct_app.auth.models import User, db
-from mct_app.auth.forms import LoginForm
+from mct_app.auth.forms import RegistrationForm, LoginForm
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/registration', methods=['GET', 'POST'])
+def registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Вы успешно зарегистрировались на сайте')
+        print('Успешно')
+        return redirect('account')
+    if form.errors:
+        print(form.username.errors)
+        flash('Вы НЕ зарегистрировались на сайте')
+    return render_template('forms/registration.html', form=form)
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        session['phone'] = form.phone.data
-        return redirect(url_for('site.home'))
+        flash('Вы успешно зашли на сайт')
+        return redirect('account')
     return render_template('forms/login.html', form=form)
 
+@auth.route('/account')
+def account():
+    return render_template('account.html')
