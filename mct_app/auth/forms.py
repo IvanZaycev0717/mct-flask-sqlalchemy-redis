880e161effc0a5423904e17b0a26e62e70b884a3
Ivan Zaytsev <ivzaycev0717@yandex.ru>
2024-05-03 20:12:33 +0300

    login and red done

--- mct_app/auth/forms.py
+++ mct_app/auth/forms.py
@@ -4,10 +4,26 @@ from wtforms import StringField, PasswordField, EmailField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError, Optional
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired(message='Требуется ввести имя пользователя')])
-    password = PasswordField('Пароль', validators=[DataRequired(message='Требуется ввести пароль')])
-    email = EmailField('Адрес электронной почты', validators=[DataRequired(message='Требуется ввести адрес электронной почты'), Email(message='Неверно введён адрес электронной почты')])
-    phone = StringField('Номер телефона', validators=[Optional()])
+    username = StringField(
+        'Имя пользователя',
+        render_kw={"placeholder": "Придумайте имя пользователя"},
+        validators=[DataRequired(message='Требуется ввести имя пользователя')]
+        )
+    password = PasswordField(
+        'Пароль',
+        render_kw={"placeholder": "Придумайте пароль"},
+        validators=[DataRequired(message='Требуется ввести пароль')]
+        )
+    email = EmailField(
+        'Адрес электронной почты',
+        render_kw={"placeholder": "Введите адрес электронной почты"},
+        validators=[DataRequired(message='Требуется ввести адрес электронной почты'),
+                    Email(message='Неверно введён адрес электронной почты')]
+                    )
+    phone = StringField(
+        'Номер телефона',
+        validators=[Optional()]
+        )
     submit = SubmitField('Зарегистрироваться')
 
     def validate_phone(self, phone):
@@ -19,6 +35,14 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Неправильно введен номер телефона')
 
 class LoginForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
+    username = StringField(
+        'Имя пользователя',
+        render_kw={"placeholder": "Введите имя пользователя"},
+        validators=[DataRequired(message='Требуется ввести имя пользователя')]
+        )
+    password = PasswordField(
+        'Пароль',
+        render_kw={"placeholder": "Введите пароль"},
+        validators=[DataRequired(message='Требуется ввести пароль')]
+        )
     submit = SubmitField('Войти')
