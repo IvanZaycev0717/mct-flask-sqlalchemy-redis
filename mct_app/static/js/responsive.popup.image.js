0ecb3cd978577a92768f63300a7a434b18d2b013
Ivan Zaytsev <ivzaycev0717@yandex.ru>
2024-05-29 18:55:58 +0300

    dark theme javascript fix

---
+++ mct_app/static/js/responsive.popup.image.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.article-card img').forEach(image =>{
+    image.onclick = () => {
+        document.querySelector('.popup-image').style.display = 'block';
+        document.querySelector('.popup-image img').src = image.getAttribute('src');
+    }
+});
+document.querySelector('.popup-image span').onclick = () => {
+    document.querySelector('.popup-image').style.display = 'none';
+};
+document.querySelector('.popup-image').onclick = () => {
+    document.querySelector('.popup-image').style.display = 'none';
+};
