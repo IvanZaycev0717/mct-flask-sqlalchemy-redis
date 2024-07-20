7c9bde5b3b2898a54619228db7d1fd137e0bdf9a
IvanZaycev0717 <ivzaycev0717@yandex.ru>
2024-07-21 02:52:05 +0300

    frontend fixed

--- mct_app/static/js/popover.scr.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover({
-         //trigger: 'focus',
-        trigger: 'hover',
-        html: true,
-        title: 'Toolbox'
-   }) 
-});
